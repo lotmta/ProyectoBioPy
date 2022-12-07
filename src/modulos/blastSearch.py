d9411cb5d162d9ecb499cd5f0a01a7cc31eef4ca
aldogp14 <100377560+aldogp14@users.noreply.github.com>
2022-12-07 16:25:08 -0600

    se agrega la funcion para crear archivo con la informacion de blast y para varias secuencias

--- src/modulos/blastSearch.py
+++ src/modulos/blastSearch.py
@@ -3,7 +3,7 @@ NAME
 	blastSearch.py
     
 VERSION
-    1.0
+    1.2
     
 AUTHOR
 	Aldo Garcia	
@@ -22,11 +22,18 @@ USAGE
 
 from Bio import SearchIO
 from Bio.Blast import NCBIWWW
+import io
 
-def protName(protSeq):
+def protName(protSeq, BS, i):
 
     # Corremos blastp, usando la base de datos pdb y pidiendo solo el mejor hit
-    result = NCBIWWW.qblast("blastp", "pdb", protSeq, hitlist_size=1)
+    print('Corriendo Blastp...')
+
+    # En caso de que se haya usado el parametro para guardar el resultado de Blast, guardamos todos los alineamientos
+    if BS:
+        result = NCBIWWW.qblast("blastp", "pdb", protSeq)
+    else:
+        result = NCBIWWW.qblast("blastp", "pdb", protSeq, hitlist_size=1)
     
     # Parseamos el resultado para poder trabajar con el
     result = SearchIO.read(result, 'blast-xml')
@@ -49,6 +56,27 @@ def protName(protSeq):
 
     # Se imprime a pantalla el nombre del mejor hit y el E value de la alineacion
     print(f"Tu proteina tuvo el mejor alienamiento con {protNomb}, con un E value de {result[0][0].evalue}")
+
+    # En caso de que se haya usado el parametro para guardar el resultado de blast, lo guardamos en un archivo
+    if BS:
+
+        # Se hace esto por si hay mas de una secuencia en el fasta
+        if i == 0:
+            file = io.open("BlastOutpt.txt", "w", encoding='utf-8')
+        else:
+            file = io.open(f"BlastOutpt_{i+1}.txt", "w", encoding='utf-8')
+        
+        txt= 'Blast Results:\n\n'
+
+        for alignment in result:
+            txt +=f"Sequence ID: {alignment.id}"
+            txt +=f"\nDescription: {alignment.description}"
+            txt +=f"\nE value: {alignment[0].evalue}"
+            txt +=f"\nBit Score:  {alignment[0].bitscore}"
+            txt +=f"\nAlignment:\n{alignment[0].aln}\n\n"
+        
+        file.write(txt)
+        file.close()
 
     # Regresamos el nombre probable de nuestra proteina
     return(protNomb)
