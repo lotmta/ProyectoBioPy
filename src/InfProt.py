1af5f532e67c682a62be3d9ee29aba0bbd7ec17d
lotmta <100377623+lotmta@users.noreply.github.com>
2022-12-07 01:48:10 -0600

    Version funcional del programa principal

--- src/InfProt.py
+++ src/InfProt.py
@@ -1,9 +1,37 @@
+'''
+NAME
+	InfProt.py
+    
+VERSION
+    1.0
+    
+AUTHOR
+	Lot, Andres, Aldo	
+    
+DESCRIPTION
+    Dada la secuencia de una proteína desconocida y un tema en el que se quiere ver si tiene algún rol, da el nombre
+    probable de la proteína dada usando blastp, y los artículos donde se menciona esta proteína y el tema.
+
+    Se puede usar si al hacer un estudio de proteoma en células cancerosas, por ejemplo, se ve que una proteína de la
+    cual no se sabe su nombre o función es sobre expresada.
+
+CATEGORY
+	Gene Expression
+    
+USAGE
+    py InfProt.py -f (Direccion del archivo) -t (Tema de interes)
+    py InfProt.py -f (Direccion del archivo) -t (Tema de interes) -BS (Si se quiere que se escriba un archivo con los detalles del blastp) 
+'''
+
 import sys
 from Bio import Entrez, SeqIO
 import argparse
 
-sys.path.insert(0, '.\src\modulos')
+sys.path.insert(0, '.\modulos')
 
+from blastSearch import protName
+from Articl import getArticl
+from write import wrFile
 
 parser = argparse.ArgumentParser(
     description="")
@@ -11,10 +39,13 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-f", "--file",
                     help="Introduce la direccion del archivo con la secuencia",
                     required=True)
-parser.add_argument('-BS', "BlastSave",
+parser.add_argument('-BS', "--BlastSave",
                     help="Usa este parametro si quieres que guarden los detalles del blastp en un archivo",
                     default=False,
                     action='store_true')
+parser.add_argument("-t", "--topic",
+                    help="Introduce el tema en el que quieres ver si tu proteina es relevante, cancer por ejemplo",
+                    required=True)
 args = parser.parse_args()
 
 Entrez.email = "lotmateohernandezespinosa@gmail.com"
@@ -30,3 +61,16 @@ except FileNotFoundError:
 except ValueError:
     print('Error de formato: Se encontró tu archivo, pero no se pudo abrir, asegurate de que esté en formato fasta')
     quit()
+
+# Pasamos el tema elegido a una variable
+tema = args.topic
+
+# Usamos la funcion que corre blast para encontrar el nombre (probable) de nuestra proteina
+print('Corriendo Blastp...')
+protN = protName(prot.seq)
+
+# Buscamos los articulos donde este nuestro tema y nuestra proteina
+articulos, Ids, NumArts = getArticl(tema, protN)
+
+# Con todo lo obtenido escribimos el archivo de salida
+wrFile(protN, tema, articulos, Ids, NumArts)
